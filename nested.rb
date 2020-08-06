eb420f6c640b45e6cf4521b926da2705caa2453d
Jackson Beytebiere <jacksongrowson@gmail.com>
2020-08-06 03:59:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -22,7 +22,7 @@ end
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
-	programmer_hash = :grace_hopper
+	programmer_hash =
  		{
       :grace_hopper => {
         :known_for => "COBOL",
@@ -37,7 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_kay]{:known_for}
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
