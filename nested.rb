2a47de16ae348500c2bfd96f27a510df81c78d91
Jackson Beytebiere <jacksongrowson@gmail.com>
2020-08-06 04:01:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,9 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:languages]
+arr=programmer_hash[:dennis_ritchie][:languages]
+arr.join
+return arr
 end
 
 def adding_matz
